4888dab97ea0ea1ac4ddacd0b4d243fbc1b95b93
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2023-05-05 11:53:45 +0530

    Eslint enabled

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-console.log("Hello");
-
 import moment from "moment";
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'), // May 4th 2023, 2:17:29 pm
-moment().format('dddd'),                    // Thursday
-moment().format("MMM Do YY"),             // May 4th 23
-moment().format('YYYY [escaped] YYYY'),    // 2023 escaped 2023
-moment().format());
+console.log(
+  moment().format("MMMM Do YYYY, h:mm:ss a"), // May 4th 2023, 2:17:29 pm
+  moment().format("dddd"), // Thursday
+  moment().format("MMM Do YY"), // May 4th 23
+  moment().format("YYYY [escaped] YYYY"), // 2023 escaped 2023
+  moment().format(),
+);
