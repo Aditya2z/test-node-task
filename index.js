a78af913df86cab52a5c2a07e10c52764ed7eaae
Aditya Dhanraj <pandeydhanrajaditya@gmail.com>
2023-05-08 23:29:47 +0530

    All jest test cases written

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ console.log(
   moment().format("YYYY [escaped] YYYY"), // 2023 escaped 2023
   moment().format(),
 );
+
+console.log(
+  moment().format("MMMM Do YYYY, h:mm:ss a"), // May 4th 2023, 2:17:29 pm
+  moment().format("dddd"), // Thursday
+  moment().format("MMM Do YY"), // May 4th 23
+  moment().format("YYYY [escaped] YYYY"), // 2023 escaped 2023
+  moment().format(),
+);
